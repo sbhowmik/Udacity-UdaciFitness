7efd7c4cb9abbb2ce96bcd43a4a3162577d36330
Suman Bhowmik <suman.bhowmik@yahoo.com>
2020-05-28 03:47:39 -0400

    App store preps in app.json

--- utils/helpers.js
+++ utils/helpers.js
@@ -195,7 +195,7 @@ export function setLocalNotification () {
 
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
-              tomorrow.setHours(2)
+              tomorrow.setHours(20)
               tomorrow.setMinutes(0)
 
               Notifications.scheduleLocalNotificationAsync(
